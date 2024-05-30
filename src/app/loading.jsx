a4d436b006d2b262f818a9fcb547f023d90fb17c
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-05-30 18:27:57 +0600

    [ADD] Multichain section added with responsiveness

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,7 @@
 
 import { PulseLoader } from "react-spinners";
 
-const loading = () => {
+const Loading = () => {
   return (
     <div className="w-screen h-screen overflow-hidden flex justify-center items-center">
       <PulseLoader color="#cf22f1" size={15} speedMultiplier={0.6} />
@@ -10,4 +10,4 @@ const loading = () => {
   );
 };
 
-export default loading;
+export default Loading;
