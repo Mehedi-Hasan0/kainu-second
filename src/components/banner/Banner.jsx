857f6235de9050ec4a4373341bec94bd364230cb
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-07-09 10:11:11 +0600

    [ADD] new banner content & section

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,7 +3,7 @@ import BannerTitle from "./BannerTitle";
 
 const Banner = () => {
   return (
-    <section className="bg-[url('/assets/images/banner/group-bg.png')] bg-no-repeat bg-cover bg-top  flex items-end justify-center banner-section pb-6 lg:pb-8 xl:pb-12 2xl:pb-16 overflow-x-hidden relative">
+    <section className="banner-section flex items-end justify-center banner-section pb-6 lg:pb-8 xl:pb-12 2xl:pb-16 overflow-x-hidden relative">
       <div className="main-container flex flex-col gap-16 md:gap-20 lg:gap-32 xl:gap-36 2xl:gap-48 justify-between pt-[110px] sm:pt-32 md:pt-36 lg:pt-48 xl:pt-44 2xl:pt-52 relative">
         {/* title content */}
         <BannerTitle />
